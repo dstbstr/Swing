a13ca5aed2e69df5f7e7746ab8e788b406e21b81
Dustin Randall <durandal@microsoft.com>
2025-03-01 20:12:49 -0800

    Adjustments for actual woodside sheets

--- WaiverWatcher/WaiverWatcher.ts
+++ WaiverWatcher/WaiverWatcher.ts
@@ -3,36 +3,41 @@
 import { GetSingleFolder, GetSingleFile, GetSingleSheet, GetSingleRow, IndexToHeader, FindColumnIndex} from "../Utils/SheetUtils.ts"
 import {MONTHS, PARENT_FOLDER_NAME} from "../Utils/Constants.ts"
 
-const FIRST_NAME_REGEX = /first ?name/i;
-const LAST_NAME_REGEX = /last ?name/i;
+const FIRST_NAME_REGEX = /first/i;
+const LAST_NAME_REGEX = /last/i;
 const WAIVER_REGEX = /waiver/i;
 const WAIVER_NOTES_REGEX = /know/i;
 const ATTENDENCE_NOTES_REGEX = /notes/i;
 const MINORS_REGEX = /minor/i;
 
 export default function CopyLatestWaiverToAttendance () {
-    const [firstName, lastName, notes, minors] = GetNewData();
+    const [firstName, lastName/*, notes, minors*/] = GetNewData();
     var attendenceSheet = GetAttendenceSheet();
-    UpdateAttendence(attendenceSheet, firstName, lastName, notes);
+    UpdateAttendence(attendenceSheet, firstName, lastName/*, notes*/);
+    /*
     minors.forEach(m => {
         var [minorFirstName, minorLastName] = m.split(" ", 2);
         UpdateAttendence(attendenceSheet, minorFirstName, minorLastName, notes);
     });
+    */
 }
 
-const GetNewData = (): [string, string, string, string[]] => {
+const GetNewData = (): [string, string/*, string, string[]*/] => {
     var waiverSheet = GetWaiverSheet();
     var latestRow = GetSingleRow(waiverSheet, waiverSheet.getLastRow());
     var lut = IndexToHeader(waiverSheet);
     var firstNameIdx = FindColumnIndex(lut, FIRST_NAME_REGEX);
     var lastNameIdx = FindColumnIndex(lut, LAST_NAME_REGEX);
+    /*
     var notesIdx = FindColumnIndex(lut, WAIVER_NOTES_REGEX);
     var minorsIdx = FindColumnIndex(lut, MINORS_REGEX);
     var minors = latestRow[minorsIdx]
         .split("\n")
         .map(m => m.trim())
         .filter(m => m.length > 0);
-    return [latestRow[firstNameIdx], latestRow[lastNameIdx], latestRow[notesIdx], minors];
+        */
+    //return [latestRow[firstNameIdx], latestRow[lastNameIdx], latestRow[notesIdx], minors];
+    return [latestRow[firstNameIdx], latestRow[lastNameIdx]];
 };
 
 const GetAttendenceSheet = () : GoogleAppsScript.Spreadsheet.Sheet => {
@@ -64,29 +69,27 @@ const FindExistingIndex = (sheet: GoogleAppsScript.Spreadsheet.Sheet, firstName:
     return undefined;
 };
 
-const UpdateAttendence = (sheet: GoogleAppsScript.Spreadsheet.Sheet, firstName: string, lastName: string, notes: string) => {
+const UpdateAttendence = (sheet: GoogleAppsScript.Spreadsheet.Sheet, firstName: string, lastName: string/*, notes: string*/) => {
     var lut = IndexToHeader(sheet);
     var firstNameIdx = FindColumnIndex(lut, FIRST_NAME_REGEX);
     var lastNameIdx = FindColumnIndex(lut, LAST_NAME_REGEX);
     var waiverIdx = FindColumnIndex(lut, WAIVER_REGEX);
-    var notesIdx = FindColumnIndex(lut, ATTENDENCE_NOTES_REGEX);
+    //var notesIdx = FindColumnIndex(lut, ATTENDENCE_NOTES_REGEX);
     var existingIndex = FindExistingIndex(sheet, firstName, lastName, firstNameIdx, lastNameIdx);
     if (existingIndex === undefined) {
         var newRow = new Array(sheet.getLastColumn());
         newRow[firstNameIdx] = firstName;
         newRow[lastNameIdx] = lastName;
         newRow[waiverIdx] = "Yes";
-        newRow[notesIdx] = notes;
+        //newRow[notesIdx] = notes;
         sheet.appendRow(newRow);
         Logger.log(`Added new row for ${firstName} ${lastName}`);
     }
     else {
         var existingRow = GetSingleRow(sheet, existingIndex);
         existingRow[waiverIdx] = "Yes";
-        existingRow[notesIdx] = notes;
+        //existingRow[notesIdx] = notes;
         sheet.getRange(existingIndex, 1, 1, existingRow.length).setValues([existingRow]);
         Logger.log(`Updated row for ${firstName} ${lastName}`);
     }
-    sheet.setFrozenRows(1);
-    sheet.sort(lastNameIdx);
 };
