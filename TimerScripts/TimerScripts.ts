fce47e91868d6690ba18f8df28897bf255b4f6dd
Dustin Randall <durandal@microsoft.com>
2025-03-27 22:03:03 -0700

    Updated EnsureMonthExists to use utils, and reliably copy previous month's headers

--- TimerScripts/TimerScripts.ts
+++ TimerScripts/TimerScripts.ts
@@ -1,40 +1,59 @@
 import {MONTHS} from "../Utils/Constants.ts"
+//import {FindFirstDateIndex, IndexToHeader, TryGetSingleSheet} from "../Utils/SheetUtils.ts"
+//import {GetAttendenceFile} from "../Utils/WoodsideUtils.ts"
 
 const THURSDAY = 4;
 
-const EnsureMonthExists = () => {
-    var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-    var currentMonth = new Date().getMonth();
-    var monthToCreate = MONTHS[currentMonth];
-    var targetSheet = spreadsheet.getSheets().filter(function (s) { return s.getName() === monthToCreate; });
-    if (targetSheet.length > 0) {
-        Logger.log("Sheet ".concat(monthToCreate, " already exists"));
+export default function EnsureMonthExists() {
+    var file = GetAttendenceFile();
+    var month = new Date().getMonth();
+    const currentMonth = TryGetSingleSheet(file, MONTHS[month]);
+    const previousMonth = TryGetSingleSheet(file, MONTHS[month - 1]);
+    if(currentMonth !== undefined) {
+        Logger.log(`Sheet ${MONTHS[month]} already exists`);
         return;
     }
-    var newSheet = spreadsheet.insertSheet(monthToCreate, currentMonth);
-    var headerValues = GetHeaderNames();
+    if(previousMonth === undefined) {
+        Logger.log(`Could not find ${MONTHS[month - 1]} sheet`);
+        return;
+    }
+
+    var newSheet = file.insertSheet(MONTHS[month]);
+    const nonDateHeaders = GetNonDateHeaders(previousMonth);
+    const dateHeaders = GetDateHeaders();
+
+    const headerValues = nonDateHeaders.concat(dateHeaders);
+
     newSheet.appendRow(headerValues);
     var headerRow = newSheet.getRange(1, 1, 1, headerValues.length);
     headerRow.setFontWeight("bold");
     newSheet.setFrozenRows(1);
-    var previousSheet = GetPreviousSheet(spreadsheet, currentMonth);
-    if (previousSheet === undefined) {
-        Logger.log("Could not find previous month");
-        return;
-    }
-    else {
-        CopyPreviousMonth(newSheet, previousSheet);
-    }
+
+    CopyPreviousMonth(newSheet, previousMonth, nonDateHeaders.length);
 };
 
-const GetHeaderNames = (): string[] => {
-    var date = new Date();
-    var currentYear = date.getFullYear();
-    var currentMonth = date.getMonth();
-    var daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
-    var firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
-    var firstThursday = ((11 - firstDayOfMonth) % 7) + 1;
-    var result = ["First Name", "Last Name", "Notes", "Waiver"];
+const GetNonDateHeaders = (sheet: GoogleAppsScript.Spreadsheet.Sheet): string[] => {
+    const lut = IndexToHeader(sheet);
+    const firstDateHeader = FindFirstDateIndex(lut);
+    let result: string[] = [];
+    Object.entries(lut).forEach(([header, index], _) => {
+        if (index < firstDateHeader) {
+            result[index] = header;
+        }
+    });
+
+    return result;    
+}
+
+const GetDateHeaders = (): string[] => {
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const currentMonth = date.getMonth();
+    const daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+    const firstThursday = ((11 - firstDayOfMonth) % 7) + 1;
+    let result: string[] = [];
+    
     for (var day = firstThursday; day <= daysInMonth; day += 7) {
         const month = `${currentMonth + 1}`.padStart(2, "0");
         const dayStr = `${day}`.padStart(2, "0");
@@ -43,21 +62,9 @@ const GetHeaderNames = (): string[] => {
     return result;
 };
 
-const GetPreviousSheet = (sheet, currentMonth): GoogleAppsScript.Spreadsheet.Sheet | undefined => {
-    if (currentMonth < 1) {
-        return undefined;
-    }
-    var previousMonth = MONTHS[currentMonth - 1];
-    var result = sheet.getSheets().filter(function (s) { return s.getName() === previousMonth; });
-    if (result.length === 0) {
-        return undefined;
-    }
-    return result[0];
-};
-
-const CopyPreviousMonth = (current, previous) => {
+const CopyPreviousMonth = (current: GoogleAppsScript.Spreadsheet.Sheet, previous: GoogleAppsScript.Spreadsheet.Sheet, headerCount: number) => {
     var previousData = previous.getDataRange().getValues();
     previousData.slice(1).forEach(function (row) {
-        current.appendRow(row.slice(0, 4));
+        current.appendRow(row.slice(0, headerCount));
     });
 };
